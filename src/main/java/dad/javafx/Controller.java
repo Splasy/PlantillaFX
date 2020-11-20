68b903cee72343261f99ba838f9577ffc994c047
Aitor <55486830+Splasy@users.noreply.github.com>
2020-11-20 09:51:47 +0000

    plantilla subida

---
+++ src/main/java/dad/javafx/Controller.java
@@ -0,0 +1,15 @@
+package dad.javafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+public class Controller extends GridPane{
+
+	@FXML
+	private GridPane view;
+	
+	
+	public GridPane getView() {
+		return view;
+	}
+}
